7d281d9ac9c7356ba01dd2809710e63f337f56fb
Oliver Nordbjerg <hi@notbjerg.me>
2021-05-22 10:32:42 +0200

    Update fish config

--- fish/.config/fish/conf.d/greeting.fish
+++ fish/.config/fish/conf.d/greeting.fish
@@ -1,5 +1,2 @@
 function fish_greeting
-  if type -q neofetch
-    neofetch --caca $HOME/Pictures/Wallpapers/term.png --crop_mode fill
-  end
 end
--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,47 +1,61 @@
-# name: RobbyRussel
-#
-# You can override some default options in your config.fish:
-#   set -g theme_display_git_untracked no
+# name: clearance
+# ---------------
+# Based on idan. Display the following bits on the left:
+# - Virtualenv name (if applicable, see https://github.com/adambrenecki/virtualfish)
+# - Current directory name
+# - Git branch and dirty state (if inside a git repo)
 
 function _git_branch_name
   echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
 end
 
-function _is_git_dirty
-  set -l show_untracked (git config --bool bash.showUntrackedFiles)
-  set untracked ''
-  if [ "$theme_display_git_untracked" = 'no' -o "$show_untracked" = 'false' ]
-    set untracked '--untracked-files=no'
-  end
-  echo (command git status -s --ignore-submodules=dirty $untracked 2> /dev/null)
+function _git_is_dirty
+  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
 end
 
 function fish_prompt
   set -l last_status $status
-  set -l cyan (set_color -o cyan)
-  set -l yellow (set_color -o yellow)
-  set -l red (set_color -o red)
-  set -l blue (set_color -o blue)
-  set -l green (set_color -o green)
+
+  set -l cyan (set_color cyan)
+  set -l yellow (set_color yellow)
+  set -l red (set_color red)
+  set -l blue (set_color blue)
+  set -l green (set_color green)
   set -l normal (set_color normal)
 
-  if test $last_status = 0
-      set arrow "$green➜ "
-  else
-      set arrow "$red➜ "
-  end
-  set -l cwd $cyan(basename (prompt_pwd))
+  set -l cwd $blue(pwd | sed "s:^$HOME:~:")
 
+  # Output the prompt, left to right
+
+  # Add a newline before new prompts
+  echo -e ''
+
+  # Display [venvname] if in a virtualenv
+  if set -q VIRTUAL_ENV
+      echo -n -s (set_color -b cyan black) '[' (basename "$VIRTUAL_ENV") ']' $normal ' '
+  end
+
+  # Print pwd or full path
+  echo -n -s $cwd $normal
+
+  # Show git branch and status
   if [ (_git_branch_name) ]
-    set -l git_branch $red(_git_branch_name)
-    set git_info "$blue git:($git_branch$blue)"
+    set -l git_branch (_git_branch_name)
 
-    if [ (_is_git_dirty) ]
-      set -l dirty "$yellow ✗"
-      set git_info "$git_info$dirty"
+    if [ (_git_is_dirty) ]
+      set git_info '(' $yellow $git_branch "±" $normal ')'
+    else
+      set git_info '(' $green $git_branch $normal ')'
     end
+    echo -n -s ' · ' $git_info $normal
   end
 
-  echo -n -s $arrow ' ' $cwd $git_info $normal ' '
-end
+  set -l prompt_color $red
+  if test $last_status = 0
+    set prompt_color $normal
+  end
 
+  # Terminate with a nice prompt char
+  echo -e ''
+  echo -e -n -s $prompt_color '⟩ ' $normal
+end
