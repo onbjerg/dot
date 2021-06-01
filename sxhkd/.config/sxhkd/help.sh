81506922bc83b9e9c7e2a41bc40ff7ad1f81588e
Oliver Nordbjerg <hi@notbjerg.me>
2021-06-01 23:03:38 +0200

    Change launch and help prompt styles

--- sxhkd/.config/sxhkd/help.sh
+++ sxhkd/.config/sxhkd/help.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc |
+awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc |
     column -t -s $'\t' |
-    rofi -dmenu -i -markup-rows -no-show-icons -width 1000 -lines 15 -yoffset 40
+    rofi -dmenu -i -markup-rows -no-show-icons
