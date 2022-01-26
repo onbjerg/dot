c42f999998138110f593ba3d63200cb9e53e04d2
Oliver Nordbjerg <hi@notbjerg.me>
2022-01-26 10:58:49 +0100

    Remove help script

--- sxhkd/.config/sxhkd/help.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc |
-    column -t -s $'\t' |
-    rofi -dmenu -i -markup-rows -no-show-icons
