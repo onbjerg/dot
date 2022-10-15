cce96b5df1209b4b7d724f75c914596a00c502a9
Oliver Nordbjerg <hi@notbjerg.me>
2022-10-15 09:41:35 +0200

    refactor: simplify `ws`

--- dot_config/fish/functions/ws.fish
+++ dot_config/fish/functions/ws.fish
@@ -1,28 +1,15 @@
 function ws
 	set -l repo $argv[1]
 	set -l hub $argv[2]
-	set -q argv[2]; or set -l hub "github"
 
 	if test -z "$repo"
 		echo "Please set a repository name."
 		return 1
 	end
 
-	switch $argv[2]
-		case bjerger
-			set hub bjerger
-			set hub_host git.bjerger.xyz
-		case aur
-			set hub aur
-			set hub_host aur.archlinux.org
-		case '*'
-			set hub github
-			set hub_host github.com
+	if not test -d $HOME/Projects/$repo
+		mkdir -p $HOME/Projects/$repo
+		git clone git@github.com:$argv $HOME/Projects/$repo
 	end
-
-	if not test -d $HOME/Projects/$hub/$repo
-		mkdir -p $HOME/Projects/$hub/$repo
-		git clone git@$hub_host:$argv $HOME/Projects/$hub/$repo
-	end
-	cd $HOME/Projects/$hub/$repo
+	cd $HOME/Projects/$repo
 end
