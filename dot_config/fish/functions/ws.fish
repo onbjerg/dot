e75263707c7622ab0e88322e6917b880d7872d9e
Oliver Nordbjerg <hi@notbjerg.me>
2022-11-28 21:12:24 +0100

    chore: move projects folder

--- dot_config/fish/functions/ws.fish
+++ dot_config/fish/functions/ws.fish
@@ -2,14 +2,19 @@ function ws
 	set -l repo $argv[1]
 	set -l hub $argv[2]
 
+	set -l root $HOME/Projects
+	if test -d /media/vault/Projects
+		set root /media/vault/Projects
+	end
+
 	if test -z "$repo"
 		echo "Please set a repository name."
 		return 1
 	end
 
-	if not test -d $HOME/Projects/$repo
-		mkdir -p $HOME/Projects/$repo
-		git clone git@github.com:$argv $HOME/Projects/$repo
+	if not test -d $root/$repo
+		mkdir -p $root/$repo
+		git clone git@github.com:$argv $root/$repo
 	end
-	cd $HOME/Projects/$repo
+	cd $root/$repo
 end
