33d13ec3cd478496690d88c7e6f6593b318b4fee
Oliver Nordbjerg <hi@notbjerg.me>
2022-11-28 22:45:16 +0100

    refactor: move rust/cargo

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,16 +1,14 @@
+# fnm
+set PATH /home/oliver/.fnm $PATH
+fnm env --shell fish | source
+
+# Rust
+set PATH $HOME/.local/share/cargo/bin $PATH
+
+# Foundry
+set PATH $HOME/.foundry/bin $PATH
+
 if status is-interactive
-    # fnm
-    set PATH /home/oliver/.fnm $PATH
-    fnm env --shell fish | source
-
-    # Rust
-    set RUSTUP_HOME $HOME/.local/share/rustup
-    set CARGO_HOME $HOME/.local/share/cargo
-    set PATH $HOME/.local/share/cargo/bin $PATH
-
-    # Foundry
-    set PATH $HOME/.foundry/bin $PATH
-
     # Prompt
     starship init fish | source
 end
