8c01e69d8e5ad7c257013ae86535d66b51a4bf2a
Oliver Nordbjerg <hi@notbjerg.me>
2022-12-20 22:17:40 +0100

    feat: jetbrains toolbox scripts

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -8,6 +8,9 @@ set PATH $HOME/.local/share/cargo/bin $PATH
 # Foundry
 set PATH $HOME/.foundry/bin $PATH
 
+# JetBrains
+set PATH $HOME/.local/share/JetBrains/Toolbox/scripts $PATH
+
 if status is-interactive
     # Prompt
     starship init fish | source
